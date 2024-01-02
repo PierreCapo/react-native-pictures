cc070a26659764379fa01920ed1a09f8fbe135db
PierreCapo <pierre.caporossi@gadz.org>
2024-01-02 13:30:20 +0100

    refactor: use matrix instead of uiscrollview

--- ios/PicturesViewManager.swift
+++ ios/PicturesViewManager.swift
@@ -13,7 +13,11 @@ class PicturesViewManager: RCTViewManager {
 class PicturesView: UIView {
     
     let imageView = UIImageView()
-    let scrollView = UIScrollView()
+    var animationDuration: CGFloat = 0.3
+    
+    var pan = UIPanGestureRecognizer()
+    var pinch = UIPinchGestureRecognizer()
+    var doubleTap = UITapGestureRecognizer()
     
     @objc var imageUrl: String? = nil {
         didSet {
@@ -34,60 +38,112 @@ class PicturesView: UIView {
     }
     
     func layoutContent() {
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        self.addSubview(scrollView)
-        scrollView.addSubview(imageView)
         imageView.contentMode = .scaleAspectFit
+        self.addSubview(imageView)
         
         NSLayoutConstraint.activate([
-            scrollView.topAnchor.constraint(equalTo: self.topAnchor),
-            scrollView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
-            scrollView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
-            scrollView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
-            imageView.topAnchor.constraint(equalTo: scrollView.topAnchor),
-            imageView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
-            imageView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
-            imageView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
-            imageView.heightAnchor.constraint(equalTo: scrollView.heightAnchor),
-            imageView.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
+            imageView.topAnchor.constraint(equalTo: self.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
         ])
         
-        scrollView.maximumZoomScale = 4
-        scrollView.minimumZoomScale = 1
-        scrollView.delegate = self
-        
-        let doubleTapGest = UITapGestureRecognizer(target: self, action: #selector(handleDoubleTap(_:)))
-        doubleTapGest.numberOfTapsRequired = 2
-        scrollView.addGestureRecognizer(doubleTapGest)
+        setDoubleTap()
+        setPan()
+        setPinch()
     }
     
-    @objc func handleDoubleTap(_ recognizer: UITapGestureRecognizer) {
-        let scale = min(scrollView.zoomScale * 2, scrollView.maximumZoomScale)
-        
-        if scale != scrollView.zoomScale { // zoom in
-            let point = recognizer.location(in: imageView)
-            
-            let scrollSize = scrollView.frame.size
-            let size = CGSize(width: scrollSize.width / scrollView.maximumZoomScale,
-                              height: scrollSize.height / scrollView.maximumZoomScale)
-            let origin = CGPoint(x: point.x - size.width / 2,
-                                 y: point.y - size.height / 2)
-            scrollView.zoom(to:CGRect(origin: origin, size: size), animated: true)
-        } else if scale == scrollView.maximumZoomScale {
-            scrollView.zoom(to: zoomRectForScale(scale: scrollView.maximumZoomScale, center: recognizer.location(in: imageView)), animated: true)
+    func setPan() {
+        pan = UIPanGestureRecognizer(target: self, action: #selector(self.onPan(_:)))
+        self.addGestureRecognizer(pan)
+    }
+    
+    func setPinch() {
+        pinch = UIPinchGestureRecognizer(target: self, action: #selector(self.onPinch(_:)))
+        self.addGestureRecognizer(pinch)
+    }
+    
+    func setDoubleTap() {
+        doubleTap = UITapGestureRecognizer(target: self, action: #selector(self.doubleTap(_:)))
+        doubleTap.numberOfTapsRequired = 2
+        self.addGestureRecognizer(doubleTap)
+    }
+    
+    @objc
+    func doubleTap(_ sender: UITapGestureRecognizer? = nil) {
+        guard let point = sender?.location(in: self.imageView) else { return }
+        if imageView.transform.a > 1 {
+            UIView.animate(withDuration: animationDuration, animations: {
+                self.imageView.transform = CGAffineTransform.identity
+            })
+        } else {
+            let translationMatrix = CGAffineTransform(translationX:imageView.center.x - point.x, y: imageView.center.y - point.y)
+            let scaleMatrix = CGAffineTransform(scaleX: 2, y: 2)
+            UIView.animate(withDuration: animationDuration, animations: {
+                self.imageView.transform = scaleMatrix.concatenating(translationMatrix)
+            })
         }
     }
     
-    func zoomRectForScale(scale: CGFloat, center: CGPoint) -> CGRect {
-        var zoomRect = CGRect.zero
-        zoomRect.size.height = imageView.frame.size.height / scale
-        zoomRect.size.width  = imageView.frame.size.width  / scale
-        let newCenter = scrollView.convert(center, from: imageView)
-        zoomRect.origin.x = newCenter.x - (zoomRect.size.width / 2.0)
-        zoomRect.origin.y = newCenter.y - (zoomRect.size.height / 2.0)
-        return zoomRect
+    @objc
+    func onPan(_ sender: UIPanGestureRecognizer? = nil) {
+        guard let sender else { return }
+        
+        let translation = sender.translation(in: imageView)
+        imageView.transform = imageView.transform.translatedBy(x: translation.x, y: translation.y)
+        sender.setTranslation(CGPoint(x: 0, y: 0), in: imageView)
+        
+        switch sender.state {
+        case .ended:
+            moveToViewportIfNeeded()
+        default:
+            return
+        }
     }
+    
+    func moveToViewportIfNeeded() {
+        guard let image = imageView.image else { return }
+        let aspectRatio: CGFloat = image.size.width / image.size.height
+        if imageView.transform.a < 1 {
+            UIView.animate(withDuration: 0.1, animations: {
+                self.imageView.transform = CGAffineTransform.identity
+            })
+        } else {
+            let minWidth = -(imageView.transform.a - 1) * imageView.bounds.width / 2
+            let maxWidth = (imageView.transform.a - 1) * imageView.bounds.width / 2
+            let minHeight = -(imageView.transform.a - 1) * imageView.bounds.width / (2 * aspectRatio)
+            let maxHeight = (imageView.transform.a - 1) * imageView.bounds.width / (2 * aspectRatio)
+            UIView.animate(withDuration: animationDuration, animations: { [weak self] in
+                guard let self else { return }
+                self.imageView.transform.tx = clamp(self.imageView.transform.tx, minValue: minWidth, maxValue: maxWidth)
+                self.imageView.transform.ty = clamp(self.imageView.transform.ty, minValue: minHeight, maxValue: maxHeight)
+            })
+        }
+    }
+    
+    @objc
+    func onPinch(_ sender: UIPinchGestureRecognizer? = nil) {
+        guard let sender else { return }
+        let imageBounds = imageView.bounds
+        let pinchCenter = sender.location(in: imageView)
+        let translateX = pinchCenter.x - CGRectGetMidX(imageBounds)
+        let translateY = pinchCenter.y - CGRectGetMidY(imageBounds)
+        let startTranslateTransform = CGAffineTransform(translationX: -translateX, y: -translateY)
+        let scaleTransform = CGAffineTransform(scaleX: sender.scale, y: sender.scale)
+        let endTranslateTransform = CGAffineTransform(translationX: translateX, y: translateY)
+        imageView.transform = imageView.transform.concatenating(startTranslateTransform).concatenating(scaleTransform).concatenating(endTranslateTransform)
+        
+        sender.scale = 1.0
+        
+        switch sender.state {
+        case .ended:
+            moveToViewportIfNeeded()
+        default:
+            return
+        }
+    }
+    
     
     func loadImage(imageUrl: String?) async throws {
         guard let imageUrl else {
@@ -114,32 +170,8 @@ class PicturesView: UIView {
     }
 }
 
-extension PicturesView: UIScrollViewDelegate {
-    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-        return imageView
-    }
-    
-    func scrollViewDidZoom(_ scrollView: UIScrollView) {
-        if scrollView.zoomScale > 1 {
-            if let image = imageView.image {
-                let ratioW = imageView.frame.width / image.size.width
-                let ratioH = imageView.frame.height / image.size.height
-                
-                let ratio = ratioW < ratioH ? ratioW : ratioH
-                let newWidth = image.size.width * ratio
-                let newHeight = image.size.height * ratio
-                let conditionLeft = newWidth*scrollView.zoomScale > imageView.frame.width
-                let left = 0.5 * (conditionLeft ? newWidth - imageView.frame.width : (scrollView.frame.width - scrollView.contentSize.width))
-                let conditioTop = newHeight*scrollView.zoomScale > imageView.frame.height
-                
-                let top = 0.5 * (conditioTop ? newHeight - imageView.frame.height : (scrollView.frame.height - scrollView.contentSize.height))
-                
-                scrollView.contentInset = UIEdgeInsets(top: top, left: left, bottom: top, right: left)
-            }
-        } else {
-            scrollView.contentInset = .zero
-        }
-    }
+func clamp<T>(_ value: T, minValue: T, maxValue: T) -> T where T : Comparable {
+    return min(max(value, minValue), maxValue)
 }
 
 enum ImageViewerError: Error {
